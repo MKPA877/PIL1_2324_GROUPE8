f7567359c1d78d65485935a4367447ec60b36920
Shalom8587 <houssouprodige7@gmail.com>
2024-06-07 15:24:28 +0100

    Prodige

---
+++ meeting-app/app/src/screens/Profile.jsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from "react-native";
+
+function ProfileScreen () {
+    return (
+        <SafeAreaView>
+            <Text>Profile</Text>
+        </SafeAreaView>
+    )
+}
+
+
+export default ProfileScreen
