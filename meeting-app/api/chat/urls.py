34ad5979dc2c18cac0f7e469a893e07745cbc0bf
MKPA877 <adanlienclounonprecieux877@gmail.com>
2024-06-12 17:43:43 +0100

    Round 3

--- meeting-app/api/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignInView, SignUpView
-
-
-urlpatterns = [
-    path('signin/', SignInView.as_view()),
-    path('signup/', SignUpView.as_view()),
-]
