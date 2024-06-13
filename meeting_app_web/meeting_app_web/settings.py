78f8018c577409da0be47f56b7105e60b03c3491
Shalom8587 <houssouprodige7@gmail.com>
2024-06-13 11:21:01 +0100

    Base de donnée 1

--- meeting_app_web/meeting_app_web/settings.py
+++ meeting_app_web/meeting_app_web/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = "meeting_app_web.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'user',
+        'PASSWORD': 'user229@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
